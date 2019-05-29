23b93641276bfa039fc0b865366d9173899f4471
zhj2159815 <zhj2159815@163.com>
2019-05-29 14:05:57 +0800

    增加消息格式拼接类

---
+++ DingPush/DingPush/MsgContent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DingPush
+{
+    public class MsgContent
+    {
+        public string title { get; set; }
+
+        public string text { get; set; }
+
+        public string time { get; set; }
+
+        public string type { get; set; }
+
+        public string ip { get; set; }
+
+        public string platform { get; set; }
+
+    }
+}
